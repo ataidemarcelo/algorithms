a6771d1b6e5dc3f8aa1ffe467160b46aa59185ff
Marcelo Ataíde <msa.trybe@gmail.com>
2023-04-26 15:41:08 -0300

    Requisito 2 - implementa a função `is_palindrome_recursive`

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,29 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    if low_index >= high_index:
+        return True
+
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return False
+
+
+# word = "I"
+# word = "GG"  # -> True
+# word = "ANA"  # -> True
+# word = "ESSE"  # -> True
+# word = "SOCOS"  # -> True
+# word = "REVIVER"  # -> True
+# word = "AGUA"  # -> False
+# word = ""  # -> False
+# word = "A" * 200  # -> True
+
+# word = "acararajadadajararaca"  # -> True
+# word = "a cara rajada da jararaca"
+
+
+# if __name__ == '__main__':
+#     print(is_palindrome_recursive(word, 0, len(word) - 1))
